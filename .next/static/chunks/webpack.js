627e068c2dc1cae4c7cbef1c455a32720e3a299e
abhishek <abhishek@flyde.in>
2025-12-05 18:10:59 +0530

    new change for me

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8bcc332dcf4b3c7"; }
+/******/ 		__webpack_require__.h = function() { return "00c79d0bc635b69e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
