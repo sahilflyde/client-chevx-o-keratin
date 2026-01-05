182995e594a8d270ffa96cd6f24de68e9623c806
Blinkflo Builder <builder@blinkflo.dev>
2026-01-05 12:56:55 +0000

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,20 @@
+
+import HeroSection from "./heroSection";
+import TargetAudienceSectionComp from "./targetAudienceSection";
+import OurServices from "./ourServices";
+import GridVideo from "./GridVideo";
+import WhyChoose from "./whyChoose";
+import Logo from "./logo";
+import Tabs from "./tabs";
+import TransformSection from "./TransformSection";
+
+export const componentRegistry = {
+  "hero-section": HeroSection,
+  "target-audience": TargetAudienceSectionComp,
+  "our-services": OurServices,
+  "grid-video": GridVideo,
+  "why-choose": WhyChoose,
+  "logo-section": Logo,
+  "tabs-section": Tabs,
+  "transform-section": TransformSection
+};
